e6ac30c25a0236ad79fe01fceb3f515f66041ef8
Vlad Karnaukhov <koshel171@yandex.ru>
2020-02-18 16:26:03 +0700

    Начат процесс перехода на DrMatters/rubert_cased

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import torch
-from pytorch_pretrained_bert import BertTokenizer, BertModel, BertForMaskedLM
+from transformers import AutoTokenizer, AutoModel
 import logging
 from pandas import DataFrame
 logging.basicConfig(level=logging.INFO)
 
-modelpath = "bert-base-multilingual-cased"
-tokenizer = BertTokenizer.from_pretrained(modelpath)
+modelpath = "DrMatters/rubert_cased"
+tokenizer = AutoTokenizer.from_pretrained(modelpath)
 
-text = "[CLS] dummy. [SEP] although he had already eaten a large meal, he was still very hungry [SEP]"
+text = "Британский дизайнер и предприниматель, внук королевы Великобритании Елизаветы II Дэвид Армстронг-Джонс и его супруга Серена решили подать на развод"
 
 
 def my_tokenizer(str):
@@ -24,7 +24,7 @@ def my_tokenizer(str):
 
 my_tokenized_text = my_tokenizer(text)
 
-model = BertForMaskedLM.from_pretrained(modelpath)
+model = AutoModel.from_pretrained(modelpath)
 model.eval()
 
 final_dict = dict()
@@ -43,10 +43,10 @@ for subword_index, subword in enumerate(my_tokenized_text[1]):
     tokens_tensor = torch.tensor([indexed_tokens])
     # segments_tensors = torch.tensor([segments_ids])
 
-    predictions = model(tokens_tensor)
+    predictions = model(tokens_tensor)[0]
     top_100 = list(())
     for value, ids in zip(*torch.topk(predictions[0, subword_index], 100, largest=True)):
-        predicted_token = tokenizer.convert_ids_to_tokens([ids.item()])
+        predicted_token = tokenizer.decode([ids.item()])
         top_100.append((predicted_token, value.item()))
     final_dict[subword] = top_100
     my_tokenized_text[1][subword_index] = subword
